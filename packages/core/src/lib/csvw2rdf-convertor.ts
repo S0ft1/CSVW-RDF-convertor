2ad672d25e077b1a4b065c44cb602d1ed2f9d088
Filip Ježek <filip.jezek@gmail.com>
2025-02-25 22:00:18 +0100

    insert external triples method

--- packages/core/src/lib/csvw2rdf-convertor.ts
+++ packages/core/src/lib/csvw2rdf-convertor.ts
@@ -5,7 +5,7 @@ import { CsvwTableDescription } from './types/descriptor/table.js';
 import { RDFSerialization } from './types/rdf-serialization.js';
 import { MemoryLevel } from 'memory-level';
 import { Quadstore, StoreOpts } from 'quadstore';
-import { DataFactory } from 'n3';
+import { DataFactory, Quad } from 'n3';
 import { Csvw2RdfOptions } from './conversion-options.js';
 import {
   CsvwBuiltinDatatype,
@@ -14,6 +14,7 @@ import {
 import { commonPrefixes } from './utils/prefix.js';
 import { CsvwInheritedProperties } from './types/descriptor/inherited-properties.js';
 import { CsvwColumnDescription } from './types/descriptor/column-description.js';
+import jsonld, { NodeObject } from 'jsonld';
 
 export class CSVW2RDFConvertor {
   config?: unknown;
@@ -52,24 +53,17 @@ export class CSVW2RDFConvertor {
     //throw new Error('Not implemented.');
   }
 
-  private getUri(prefix: string): Promise<string | null> {
-    return (
-      fetch(`https://prefix.cc/${prefix}.file.json`)
-        .then((response) => response.json() as Promise<PrefixCCResponse>)
-        .then((data) => data[prefix])
-        // Prefix not found, or prefix.cc does not respond
-        .catch(() => null)
-    );
-  }
-
-  private getPrefix(uri: string): Promise<string | null> {
-    return (
-      fetch(`https://prefix.cc/reverse?uri=${uri}&format=json`)
-        .then((response) => response.json() as Promise<PrefixCCResponse>)
-        .then((data) => Object.keys(data)[0])
-        // No registered prefix for the given URI, or prefix.cc does not respond
-        .catch(() => null)
-    );
+  private async insertExternalTriples(
+    store: Quadstore,
+    descriptor: DescriptorWrapper,
+    object: NodeObject
+  ) {
+    const tempDoc: NodeObject = {};
+    for (const key in descriptor.getExternalProps(object)) {
+      tempDoc[key] = object[key];
+    }
+    const rdf = (await jsonld.toRDF(tempDoc)) as Quad[];
+    await store.multiPut(rdf);
   }
 
   private interpretDatatype(
