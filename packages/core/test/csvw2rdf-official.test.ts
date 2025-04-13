5c38787b5eb4625777491501ee6c4be0e57b2004
Filip Ježek <filip.jezek@gmail.com>
2025-04-13 14:18:32 +0200

    ci hopefully ok

--- packages/core/test/csvw2rdf-official.test.ts
+++ packages/core/test/csvw2rdf-official.test.ts
@@ -35,7 +35,6 @@ describe('CSVW -> RDF Official tests', () => {
 
   for (const entry of manifest.entries
     .filter((e) => e.type === EntryType.Test)
-    .slice(-1)
     .filter((_, i) => i != 34 && (i < 65 || i > 72))) {
     test(entry.name, async () => {
       const options: Csvw2RdfOptions = {
@@ -94,7 +93,6 @@ async function runConversion(options: Csvw2RdfOptions, entry: Entry) {
 }
 
 async function loadJsonLd(path: string, base: string): Promise<string> {
-  console.log('loadJsonLd', path, base);
   const url =
     URL.parse(path, base)?.href ?? URL.parse(path)?.href ?? resolve(base, path);
   if (!isAbsolute(url) && URL.canParse(url)) {
@@ -164,7 +162,6 @@ function rdfStreamToArray(stream: Stream<Quad>) {
 
 function setupImplicit(entry: Entry) {
   fetchMock.default.mockResponse((req) => {
-    console.log('fetch', req.url);
     const url = req.url.replace(TEST_HTTP_BASE, '');
     if (entry.implicit?.includes(url)) {
       return readFile(resolve(testDir, url), 'utf-8');
