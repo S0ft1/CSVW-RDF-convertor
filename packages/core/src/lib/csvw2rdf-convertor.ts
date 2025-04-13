c83cb4a7d2d0ca2e6137b53ceeebf1a51374cdc5
Filip Ježek <filip.jezek@gmail.com>
2025-04-13 13:48:02 +0200

    debug ci

--- packages/core/src/lib/csvw2rdf-convertor.ts
+++ packages/core/src/lib/csvw2rdf-convertor.ts
@@ -97,6 +97,7 @@ export class CSVW2RDFConvertor {
    */
   public async convertFromCsvUrl(url: string) {
     const [json, resolvedUrl] = await this.resolveMetadata(url);
+    console.log('Resolved metadata', resolvedUrl, json);
     const wrapper = await normalizeDescriptor(json, this.options, resolvedUrl);
     this.options.baseIRI = resolvedUrl;
     const tablesWithoutUrl = Array.from(wrapper.getTables()).filter(
