2b5462328f86efc2f1f9861bbcab51ce205b6c9e
Jakub Yaghob <kuba.y@volny.cz>
2025-05-07 15:36:42 +0200

    number formating implemented

---
+++ packages/core/src/lib/utils/number-formating.ts
@@ -0,0 +1,125 @@
+import { DescriptorWrapper } from "../descriptor.js";
+import { ColumnDescriptionWithDataTypeAndFormat, CsvwColumnDescription } from "../types/descriptor/column-description.js";
+import { CsvwDatatype, CsvwNumberFormat } from "../types/descriptor/datatype.js";
+import { IssueTracker } from "./issue-tracker.js";
+
+function findFormatedColumns(allColumns: CsvwColumnDescription[]) {
+    let castedColumns = [];
+    for (const column of allColumns) {
+      if (column.datatype) {
+        let dataType = column.datatype as CsvwDatatype;
+        if (dataType.format) {
+          const format = dataType.format as CsvwNumberFormat;
+          if (format.pattern) {
+            castedColumns.push(column as ColumnDescriptionWithDataTypeAndFormat)
+          }
+        }
+      }
+    }
+    return castedColumns;
+  }
+
+  function patternIsValid(pattern: string): boolean {
+    //This probably doesnt encapsulate all the rules of a valid pattern.
+    let hasDecimal = false;
+    let hasGrouping = false;
+    for (const char of pattern) {
+      if (char !== '#' && char != '0' && char != '.' && char != ',') {
+        return false;
+      }
+      if (char === '.') {
+        if (hasDecimal) {
+          return false;
+        }
+        hasDecimal = true;
+      } else if (char === ',') {
+        if (hasGrouping) {
+          return false;
+        }
+        hasGrouping = true;
+      }
+    }
+    return true;
+  }
+
+  function transformNumber(value: string, columnDescription: ColumnDescriptionWithDataTypeAndFormat,issueTracker: IssueTracker): string {
+    if (columnDescription.datatype.format.pattern) {
+      if (!patternIsValid(columnDescription.datatype.format.pattern)) {
+        issueTracker.addError(`Invalid pattern: ${columnDescription.datatype.format.pattern} take a look at https://www.unicode.org/reports/tr35/tr35-39/tr35-numbers.html#Number_Patterns`, true);
+      }
+      let splitted = value.split('.');
+      if (splitted.length == 1) {
+        return value;
+      }
+      else if (splitted.length == 2) {
+        const decimalChar = columnDescription.datatype.format.decimalChar || '.';
+        const groupChar = columnDescription.datatype.format.groupChar || ',';
+        const pattern = columnDescription.datatype.format.pattern;
+        const decimalPart = splitted[1];
+        const integerPart = splitted[0];
+        let splittedPattern = pattern.split('.');
+        let integerPattern = splittedPattern[0];
+        let decimalPattern = splittedPattern[1];
+        let reversedIntegerPart = integerPart.split('').reverse().join('');
+        let reversedIntegerPattern = integerPattern.split('').reverse().join('');
+        return transformNumberInner(reversedIntegerPattern, reversedIntegerPart, groupChar).split('').reverse().join('') +
+          decimalChar +
+          transformNumberInner(decimalPattern,decimalPart, groupChar);
+      }
+      else {
+        issueTracker.addError(`Too many decimal characters in value: ${value}`, true);
+        return value;
+      }
+    }
+    return value;
+  }
+
+  function transformNumberInner(pattern: string, number: string, groupChar: string): string {
+    let transformedPart = "";
+    let patternLongerThanNumber = false;
+    let numberIndex =0;
+    for (let i = 0; i < pattern.length; i++) {
+      if (numberIndex == number.length) {
+        patternLongerThanNumber = true;
+      }
+      if (pattern[i] == '0') {
+        transformedPart += patternLongerThanNumber ? '0' : number[numberIndex];
+        numberIndex++;
+      }
+      else if (pattern[i] == '#' && !patternLongerThanNumber) {
+        transformedPart += number[numberIndex] || '';
+        numberIndex++;
+      }
+      else if (pattern[i] == ',' && !patternLongerThanNumber) {
+        transformedPart += groupChar;
+      }
+    }
+/*
+    if (!patternLongerThanNumber) {
+      //add the rest of the number
+      //maybe this should not happend in case of decimal part, as shown in the table https://www.unicode.org/reports/tr35/tr35-numbers.html#Number_Format_Patterns
+      transformedPart += number.substring(pattern.length);
+    }*/
+    return transformedPart;
+
+  }
+  
+ export function transformNumbersInTable(jsonTable: string, descriptor: DescriptorWrapper, issueTracker: IssueTracker): string {
+    const rgxForNumberRowInJson = `\\s*:\\s*"\\d*\\.*\\d*"`;
+    const regexForNumberInJson = /"(\d*\.?\d*)"/;
+    let colums = descriptor.descriptor.tableSchema?.columns;
+    if (colums) {
+      let formatedColumns = findFormatedColumns(colums);
+      for (const column of formatedColumns) {
+        if (column.name) {
+          const regex = new RegExp(`"${column.name}"${rgxForNumberRowInJson}`, 'g');
+          jsonTable = jsonTable.replace(regex, (match) => {
+            const value = match.match(regexForNumberInJson)?.[1] || '';
+            const transformedValue = transformNumber(value, column,issueTracker);
+            return match.replace(value, transformedValue);
+          });
+        }
+      }
+    }
+    return jsonTable;
+  }
