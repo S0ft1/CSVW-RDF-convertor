2aa0870481a50680493a7e9cbffefa5540525f3a
Jakub Yaghob <kuba.y@volny.cz>
2025-05-27 22:37:23 +0200

    Fixed binding creation using DataFactory

--- packages/core/src/lib/transformation-stream.ts
+++ packages/core/src/lib/transformation-stream.ts
@@ -1,22 +1,22 @@
 import { Bindings, ResultStream } from "@rdfjs/types";
-import { Readable } from "stream";
+import { Duplex, Readable } from "stream";
 import { DescriptorWrapper } from "./descriptor.js";
 import { CsvwColumnDescription } from "./types/descriptor/column-description.js";
 import { convertColumnToDateFormattedColumn, formatDate, isDateFormatedColumn } from "./utils/date-formatting.js";
-import { DataFactory, Literal } from "n3";
+import { Literal } from "n3";
 import { CsvwTableDescriptionWithRequiredColumns } from "./rdf2csvw-convertor.js";
 import { convertColumnToNumberFormattedColumn, isNumberColumn, transformNumber } from "./utils/number-formating.js";
 import { IssueTracker } from "./utils/issue-tracker.js";
-
+import { DataFactory } from 'rdf-data-factory'
+const factory = new DataFactory();
 
 
 export function transformStream(stream: ResultStream<Bindings>, tableDescription: CsvwTableDescriptionWithRequiredColumns, columnNames: string[], issueTracker: IssueTracker): ResultStream<Bindings> {
 
     const transformedStream = new Readable({
         objectMode: true,
-        read() { }
+        read() { },
     });
-
     //This part is called when the stream is piped to another stream and gets data.
     stream.on("data", (bindings) => {
         for (let i = 0; i < tableDescription.tableSchema.columns.length && !tableDescription.tableSchema.columns[i].virtual; i++) {
@@ -29,7 +29,7 @@ export function transformStream(stream: ResultStream<Bindings>, tableDescription
                 let convertedDateColumn = convertColumnToDateFormattedColumn(column);
                 if (convertedDateColumn) {
                     let formattedValue = formatDate(value, convertedDateColumn.datatype.format);
-                    bindings = bindings.set(columnNames[i], new Literal(formattedValue));
+                    bindings = bindings.set(columnNames[i], factory.literal(formattedValue));
                     break;
                 }
             }
@@ -37,7 +37,7 @@ export function transformStream(stream: ResultStream<Bindings>, tableDescription
                 let convertedNumberColumn = convertColumnToNumberFormattedColumn(column);
                 if (convertedNumberColumn) {
                     let formattedValue = transformNumber(value, convertedNumberColumn, issueTracker);
-                    bindings = bindings.set(columnNames[i], new Literal(formattedValue));
+                    bindings = bindings.set(columnNames[i], factory.literal(formattedValue));
                     break;
                 }
             }
