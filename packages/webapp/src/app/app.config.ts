f1d192b24153c20e0eceb3de316f512360c61a4b
Filip Ježek <filip.jezek@gmail.com>
2025-07-23 20:57:26 +0200

    bugfix ci pages deployment

--- packages/webapp/src/app/app.config.ts
+++ packages/webapp/src/app/app.config.ts
@@ -4,14 +4,14 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withHashLocation()),
     provideAnimationsAsync(),
   ],
 };
